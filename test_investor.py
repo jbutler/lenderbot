d334ca4b0294ffa7501e7c86e91ba40525d64ccb
Joe Butler <joebutler88@gmail.com>
2015-05-16 22:19:41 -0700

    Add support for Travis CI

---
+++ test_investor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import investor
+import unittest
+
+
+class InvestorTest(unittest.TestCase):
+	def test_dummy_test(self):
+		self.assertTrue(True)
+
+
+if __name__ == '__main__':
+	unittest.main()
+
