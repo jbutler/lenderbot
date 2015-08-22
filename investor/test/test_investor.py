ca87673701ab4f9d7e387a12f86bfab8df097a0e
Joe Butler <joebutler88@gmail.com>
2015-08-22 13:31:00 -0700

    Add setup.py

--- investor/test/test_investor.py
+++ investor/test/test_investor.py
@@ -2,10 +2,6 @@
 
 import sys
 
-sys.path.insert(0, '.')
-sys.path.insert(0, '../')
-sys.path.insert(0, '../../')
-
 from investor import Investor
 import unittest
 
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='auto-investor',
+      version='1.0.1',
+      description='LendingClub auto-investment tool',
+      author='Joe Butler',
+      author_email='joebutler@gmail.com',
+      packages=['investor'],
+)
