948d71cb2a1f548c149abe790640133dfd0053f6
Joe Butler <joebutler88@gmail.com>
2015-09-17 19:44:54 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='auto-investor',
-      version='1.0.2',
+      version='1.0.3',
       description='LendingClub auto-investment tool',
       author='Joe Butler',
       author_email='joebutler88@gmail.com',
